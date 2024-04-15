0473be7ed70be641bc74e4519fac67aa5b2145a1
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-04-15 23:37:14 +0200

    fix: other fixes. now the code runs in behavioral simulations

--- main_behavioral.vhd
+++ main_behavioral.vhd
@@ -24,13 +24,14 @@ architecture fsm of project_reti_logiche is
     type state_type is (SETUP, FIRSTREAD, IFZERO, IFNOTZERO, PRINTCREDIBILITY, SETUPINPUT, INPUT, ENDSTATE);
     signal next_state, current_state: state_type;
 begin
-    state_reg: process (i_clk, i_rst)
+    state_reg: process (i_clk, i_rst) --need to refactor: rising edge checked only in one of the processes. ie: bring main process if else in state_reg process
     begin
         if i_rst = '1' then --or rising-edge
             o_done <= '0';
-            next_state <= SETUP; --tochange
+            --next_state <= SETUP; --tochange
             current_state <= SETUP;
         elsif rising_edge(i_clk) then
+            
             current_state <= next_state;
         else
             current_state <= current_state; --??
@@ -61,17 +62,19 @@ begin
                         next_state <= IFZERO;
                     else
                         next_state <= IFNOTZERO;
+                    end if;
                     o_mem_we <= '1';       
                     counter := "0000000000"; --downto
-                    end if;
+                    
                     
                 when IFZERO =>
                     o_mem_addr <= std_logic_vector(UNSIGNED(i_add)+UNSIGNED(counter));
                     o_mem_data <= in_number;
                     if credibility /= "00000" then
                         credibility := std_logic_vector(UNSIGNED(credibility) - 1);
-                    next_state <= PRINTCREDIBILITY;
                     end if;
+                    next_state <= PRINTCREDIBILITY;
+
                 when IFNOTZERO =>
                     o_mem_addr <= std_logic_vector(UNSIGNED(i_add)+UNSIGNED(counter));
                     o_mem_data <= in_number;
@@ -80,7 +83,7 @@ begin
                     
                 when PRINTCREDIBILITY =>
                     o_mem_addr <= std_logic_vector(UNSIGNED(i_add)+UNSIGNED(counter)+1); --address of credibility
-                    o_mem_data <= credibility;
+                    o_mem_data <= "000" & credibility;
                     counter := std_logic_vector(UNSIGNED(counter)+2);
                     next_state <= SETUPINPUT;
 
@@ -98,18 +101,18 @@ begin
                             next_state <= IFZERO;
                         else
                             next_state <= IFNOTZERO;
-                        o_mem_we <= '1';    
                         end if;
+                        o_mem_we <= '1';    
                     end if;   
                     
                 when ENDSTATE =>
                     o_done <= '1';
-        end case;
+            end case;
 
         else    --soft reset cycle
             next_state <= SETUP;
             counter := "0000000000";
-            current_state <= SETUP;
+            --current_state <= SETUP;
         end if;
    end process;
 end architecture;
