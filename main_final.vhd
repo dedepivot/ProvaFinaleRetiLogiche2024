79ef441a14254bb03ef1ffea5eac4c3147f633fe
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-05-15 19:55:30 +0200

    refactor: final code refactor

--- main_final.vhd
+++ main_final.vhd
@@ -21,8 +21,9 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture fsm of project_reti_logiche is
-    type state_type is (SETUP, ENDSTATE, FIRSTREADWRITE, SETUPIDLE, PRINTCREDIBILITY, IDLEINPUT, INPUT, IDLE);
+    type state_type is (SETUPSTART, ENDSTATE, FIRSTREADWRITE, SETUPINPUT, PRINTCREDIBILITY, INPUT, IDLE);
     signal current_state: state_type;
+    signal previous_state: state_type;
 begin
     main: process(i_clk, i_rst)
         variable in_value: std_logic_vector(7 downto 0);
@@ -32,30 +33,35 @@ begin
         if (rising_edge(i_clk)) then
             if ((i_start = '1' or rising_edge(i_start)) and i_rst = '0') then
                 case current_state is  
-                    when SETUP => 
+                    when SETUPSTART => 
                         o_mem_en <= '1';
-                        counter := "0000000000000000";
+                        counter := (others => '0');
                         if(i_k = "0000000000") then
                             current_state <= ENDSTATE;
                         else
                             o_mem_we <= '0';
                             o_mem_addr <= i_add;
-                            current_state <= SETUPIDLE;
+                            previous_state <= SETUPSTART;
+                            current_state <= IDLE;
                         end if;
 
-                    when SETUPIDLE =>
-                        current_state <= FIRSTREADWRITE;
-                        --in_value = i_mem_data
-                        --o_mem_addr <= i_add + counter 
-
+                    when IDLE =>
+                        case previous_state is
+                            when SETUPSTART =>
+                                current_state <= FIRSTREADWRITE;    
+                            when SETUPINPUT =>
+                                current_state <= INPUT;
+                            when others =>
+                                current_state <= ENDSTATE; --error state
+                        end case;
 
                     when FIRSTREADWRITE =>
                         in_value := i_mem_data;
                         o_mem_we <= '1';
                         if(in_value /= "00000000") then 
-                            credibility := "00011111"; 
+                            credibility := (7 downto 5 => '0', others => '1');
                         else
-                            credibility := "00000000";
+                            credibility := (others => '0');
                         end if;
                         current_state <= PRINTCREDIBILITY;
                         o_mem_data <= in_value;
@@ -63,24 +69,21 @@ begin
                     when PRINTCREDIBILITY =>
                         o_mem_addr <= std_logic_vector(UNSIGNED(counter)+UNSIGNED(i_add)+1);
                         o_mem_data <= credibility;
-                        --o_mem_we <= '0';
-                        current_state <= IDLEINPUT;
-                    
-                    when IDLEINPUT =>
+                        current_state <= SETUPINPUT;
+
+                    when SETUPINPUT =>
                         o_mem_we <= '0';
                         counter := std_logic_vector(UNSIGNED(counter)+2);
                         o_mem_addr <= std_logic_vector(UNSIGNED(counter)+UNSIGNED(i_add));
+                        previous_state <= SETUPINPUT;
                         current_state <= IDLE;
-
-                    when IDLE =>
-                        current_state <= INPUT;
                     
                     when INPUT =>
                         if to_integer(UNSIGNED(counter)) > (2 * (to_integer(UNSIGNED(i_k)) - 1)) then 
                             current_state <= ENDSTATE; 
                         else
                             if(i_mem_data /= "00000000") then 
-                                credibility := "00011111"; 
+                                credibility := (7 downto 5 => '0', others => '1'); 
                                 in_value := i_mem_data;
                             else
                                 if(credibility /= "00000000") then
@@ -95,13 +98,13 @@ begin
                     when ENDSTATE =>
                         o_mem_en <= '0';
                         o_done <= '1';
-                    
+
                     end case;
                 
             else --(rising_edge(i_rst) or i_rst = '1' ) --(i_start = '0') then
                 o_done <= '0';
                 o_mem_en <= '0';
-                current_state <= SETUP;
+                current_state <= SETUPSTART;
             end if;
         else
             current_state <= current_state;
